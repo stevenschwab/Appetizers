937b81f8e07f2b6249694fea18a60bb46d01cf5a
Steven Schwab <schwabs@umich.edu>
2022-12-15 22:22:02 -0600

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Steven Schwab on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
